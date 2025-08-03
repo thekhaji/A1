732d845bf806226aa06912ebc99fe9c9c2bf378f
Khojiakbar <khojiakbar_kodirov@mail.ru>
2025-08-03 10:13:43 +0900

    feat: develop showroom  login process

--- src/controllers/showroom.controller.ts
+++ src/controllers/showroom.controller.ts
@@ -1,7 +1,7 @@
 import { T } from "../libs/types/common";
 import {Request, Response} from "express";
 import MemberService from "../models/Member.service";
-import { MemberInput } from "../libs/types/member";
+import { LoginInput, MemberInput } from "../libs/types/member";
 import { MemberType } from "../libs/enums/member.enum";
 
 const showroomController: T = {};
@@ -28,13 +28,17 @@ showroomController.getLogin = (req: Request, res: Response) => {
     }
 };
 
-showroomController.processLogin = (req: Request, res: Response) => {
+showroomController.processLogin = async (req: Request, res: Response) => {
     try {
         console.log("processLogin");
-        res.send("DONE!");
+        
+        const input: LoginInput = req.body;
+        const result = await memberService.processLogin(input);
+        
+        res.send(result);
     } catch (err) {
         console.log("Error, processLogin:", err);
-        
+        res.send(err);
     }
 };
 
--- src/models/Member.service.ts
+++ src/models/Member.service.ts
@@ -1,7 +1,8 @@
 import MemberModel from "../schema/Member.model";
-import { Member, MemberInput } from "../libs/types/member";
+import { LoginInput, Member, MemberInput } from "../libs/types/member";
 import Errors, { HttpCode, Message } from "../libs/Errors";
 import { MemberType } from "../libs/enums/member.enum";
+import * as bcrypt from "bcryptjs";
 
 
 class MemberService{
@@ -14,6 +15,10 @@ class MemberService{
     public async processSignup(input: MemberInput): Promise<Member>{
         const exist = await this.memberModel.findOne({memberType: MemberType.SHOWROOM}).exec();
         if(exist) throw new Errors(HttpCode.BAD_REQUEST, Message.CREATE_FAILED);
+
+        const salt = await bcrypt.genSalt();
+        input.memberPassword = await bcrypt.hash(input.memberPassword, salt);
+
         try {
             const result = await this.memberModel.create(input);
             result.memberPassword = "";
@@ -21,6 +26,20 @@ class MemberService{
         } catch (err) {
             throw new Errors(HttpCode.BAD_REQUEST, Message.CREATE_FAILED);
         }
+    }
+
+    public async processLogin(input: LoginInput): Promise<Member>{
+        const member = await this.memberModel.findOne(
+            {memberNick: input.memberNick},
+            {memberNick: 1, memberPassword: 1}
+        ).exec();
+        if (!member) throw new Errors(HttpCode.NOT_FOUND, Message.NO_MEMBER_NICK);
+
+        const isMatch = await bcrypt.compare(input.memberPassword, member.memberPassword);
+        //const isMatch = input.memberPassword === member.memberPassword;
+        if(!isMatch) throw new Errors(HttpCode.UNATHORIZED,  Message.WRONG_PASSWORD);
+
+        return member?.toObject() as Member;
     }
 
 }
